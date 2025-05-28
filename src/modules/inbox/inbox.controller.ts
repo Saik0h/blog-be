c80b275956f679356bbd279e8a970941f0d41fd0
Saik0h <omcristhyan@gmail.com>
2025-05-28 13:04:46 -0300

    refactored inbox and added search

--- src/modules/inbox/inbox.controller.ts
+++ src/modules/inbox/inbox.controller.ts
@@ -1,13 +1,26 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Query,
+  Patch,
+} from '@nestjs/common';
 import { InboxService } from './inbox.service';
-import { CreateInboxDto } from './dto/create-inbox.dto';
-import { UpdateInboxDto } from './dto/update-inbox.dto';
 import { Prisma } from 'generated/prisma';
 import { Access } from 'src/common/decorators/access-level-decorator';
 
 @Controller('api/inbox')
 export class InboxController {
   constructor(private readonly inboxService: InboxService) {}
+
+  @Get('search')
+  @Access('restrict')
+  search(@Query('q') query: string) {
+    return this.inboxService.search(query);
+  }
 
   @Post()
   @Access('public')
@@ -25,6 +38,12 @@ export class InboxController {
   @Access('restrict')
   findOne(@Param('id') id: string) {
     return this.inboxService.findOne(+id);
+  }
+
+  @Patch(':id')
+  @Access('restrict')
+  markAsRead(@Param('id') id: string) {
+    return this.inboxService.markAsRead(+id)
   }
 
   @Delete(':id')
--- src/modules/inbox/inbox.service.ts
+++ src/modules/inbox/inbox.service.ts
@@ -1,44 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import {
+  BadRequestException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { DatabaseService } from 'src/database/database.service';
 import { Prisma } from 'generated/prisma';
 
 @Injectable()
 export class InboxService {
-  constructor(private readonly prisma: DatabaseService) { }
+  constructor(private readonly prisma: DatabaseService) {}
 
-  create(data: Prisma.InboxMessageCreateInput) {
+  async create(data: Prisma.InboxMessageCreateInput) {
     try {
-      return this.prisma.inboxMessage.create({ data });
-    }
-    catch (err) {
-      throw err
+      await this.prisma.inboxMessage.create({ data });
+      return { message: 'Message sent succesfully' };
+    } catch (err) {
+      throw err;
     }
   }
 
-  findAll() {
+  async findAll() {
     try {
-      return this.prisma.inboxMessage.findMany();
-    }
-    catch (err) {
-      throw err
+      return await this.prisma.inboxMessage.findMany();
+    } catch (err) {
+      throw err;
     }
   }
 
-  findOne(id: number) {
+  async findOne(id: number) {
     try {
-      return this.prisma.inboxMessage.findUnique({ where: { id } });
-    }
-    catch (err) {
-      throw err
+      if (!id) throw new BadRequestException('ID must be provided');
+      const mail = await this.prisma.inboxMessage.findUnique({ where: { id } });
+      if (!mail) throw new NotFoundException('Mail doesnt exist');
+      return mail;
+    } catch (err) {
+      throw err;
     }
   }
 
-  remove(id: number) {
+  async remove(id: number) {
     try {
-      return this.prisma.inboxMessage.delete({ where: { id } });
+      await this.prisma.inboxMessage.delete({ where: { id } });
+      return { message: 'Mail deleted succesfully' };
+    } catch (err) {
+      throw err;
     }
-    catch (err) {
-      throw err
-    }
+  }
+
+  async markAsRead(id: number) {
+    await this.prisma.inboxMessage.update({
+      where: { id },
+      data: { read: true },
+    });
+    return {message: `Mail with the ID: ${id} was read`};
+  }
+
+  async search(query: string) {
+    return await this.prisma.inboxMessage.findMany({
+      where: {
+        OR: [
+          { message: { contains: query, mode: 'insensitive' } },
+          { subject: { contains: query, mode: 'insensitive' } },
+          { name: { contains: query, mode: 'insensitive' } },
+        ],
+      },
+    });
   }
 }
